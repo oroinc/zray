b0a7663b82df4175abc6f9561a145de8d4d14de7
Dima Soroka <dima@magecore.com>
2014-11-04 22:38:44 -0800

    Z-Ray on Oro Configuration

---
+++ README.md
@@ -0,0 +1,4 @@
+Oro Z-Ray Extension
+===================
+
+Get a better view into Oro Platform execution flow with Z-Ray
---
+++ zray.php
@@ -0,0 +1,25 @@
+<?php
+
+$zre = new \ZRayExtension("Oro");
+
+$zre->setMetadata(array(
+	'logo' => __DIR__ . DIRECTORY_SEPARATOR . 'logo.png',
+));
+
+$zre->setEnabledAfter('Oro\Bundle\DistributionBundle\OroKernel::registerBundles');
+
+$zre->traceFunction("Symfony\Component\HttpKernel\Kernel::terminate", function(){}, function($context, &$storage) {
+    $applicationContext = $context['this'];
+    $container = $applicationContext->getContainer();
+    $cm                    = $container->get('oro_config.global');
+    $settingsFromAppConfig = $container->get('oro_config.config_definition_bag');
+
+    $currentConfiguration = [];
+    foreach ($settingsFromAppConfig->all() as $bundleAlias => $settings) {
+        foreach (array_keys($settings) as $settingName) {
+            $key = implode('.', [$bundleAlias, $settingName]);
+            $currentConfiguration[$key] = $cm->get($key);
+        }
+    }
+    $storage['OroConfiuration'][] = $currentConfiguration;
+});
