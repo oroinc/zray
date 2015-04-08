1a6390c61e352ae4c45e0a175993d6c7183a02c6
Dima Soroka <dima@magecore.com>
2015-04-07 21:44:21 -0700

    Added verification for installer

--- zray.php
+++ zray.php
@@ -13,13 +13,20 @@ $zre->traceFunction("Symfony\Component\HttpKernel\Kernel::terminate", function()
     $container = $applicationContext->getContainer();
     $cm                    = $container->get('oro_config.global');
     $settingsFromAppConfig = $container->get('oro_config.config_definition_bag');
+    $isInstalled = $container->hasParameter('installed') && $container->getParameter('installed');
 
     $currentConfiguration = [];
-    foreach ($settingsFromAppConfig->all() as $bundleAlias => $settings) {
-        foreach (array_keys($settings) as $settingName) {
-            $key = implode('.', [$bundleAlias, $settingName]);
-            $currentConfiguration[$key] = $cm->get($key);
+
+    if ($isInstalled) {
+        foreach ($settingsFromAppConfig->all() as $bundleAlias => $settings) {
+            foreach (array_keys($settings) as $settingName) {
+                $key = implode('.', [$bundleAlias, $settingName]);
+                $currentConfiguration[$key] = $cm->get($key);
+            }
         }
+    } else {
+        $currentConfiguration['installed'] = false;
     }
+
     $storage['OroConfiuration'][] = $currentConfiguration;
 });
